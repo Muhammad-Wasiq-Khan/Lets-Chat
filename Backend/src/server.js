142ec5349f2d81e65405165b4a640fca193c5f1e
Muhammad Wasiq Khan <khanwasiq694@gmail.com>
2026-09-09 19:21:35 +0500

    initial commit

---
+++ Backend/src/server.js
@@ -0,0 +1,14 @@
+import express from "express"
+import dotenv from "dotenv"
+import authRoutes from './routes/auth.route.js'
+import messagesRoutes from './routes/message.route.js'
+dotenv.config()
+const app = express()
+const port = process.env.PORT
+
+app.use("/api/auth",authRoutes)
+app.use("/api/messages",messagesRoutes)
+
+app.listen(port,()=>{
+    console.log("Server is runing on port",port)
+})
